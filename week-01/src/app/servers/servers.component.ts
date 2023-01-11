c28e06b47856af4d6f5e0ff15844b297379124c5
ar-byu <103947632+ar-byu@users.noreply.github.com>
2023-01-10 20:46:04 -0500

    Finished two-way binding lesson

--- week-01/src/app/servers/servers.component.ts
+++ week-01/src/app/servers/servers.component.ts
@@ -2,9 +2,33 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: '[app-servers]',
-  template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: 'servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Test Server';
+  username = '';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true
+    }, 2000)
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Server Name: ' + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value;
+  }
+
+  onResetUsername() {
+    this.username = '';
+  }
+
+
 
 }
