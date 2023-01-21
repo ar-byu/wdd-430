85ea30374d6b30c2e746e755e696c8b18848505f
ar-byu <103947632+ar-byu@users.noreply.github.com>
2023-01-21 16:34:32 -0500

    Modified header and nav, implemented messages

--- cms/src/app/messages/message-edit/message-edit.component.ts
+++ cms/src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'app-message-edit',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class MessageEditComponent {
 
+  @ViewChild('subject') subjectInputRef: ElementRef;
+  @ViewChild('msgText') textInputRef: ElementRef;
+  @Output() addMessageEvent = new EventEmitter<Message>();
+  currentSender: string = 'Anna';
+
+  onSendMessage() {
+    const subject = this.subjectInputRef.nativeElement.value;
+    const message = this.textInputRef.nativeElement.value;
+
+    const newMessage = new Message(15, subject, message, this.currentSender);
+    this.addMessageEvent.emit(newMessage);
+  }
+
+  onClear() {
+    this.subjectInputRef.nativeElement.value = "";
+    this.textInputRef.nativeElement.value = "";
+  }
 }
--- cms/src/app/messages/message-list/message-list.component.ts
+++ cms/src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'app-message-list',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./message-list.component.css']
 })
 export class MessageListComponent {
+  messages: Message[] = [
+    new Message(1, "Grading Request", "Hi, is it possible to round up my grade to 98? I had an issue submitting so I couldn't get it in on time.", "Anna"),
+    new Message(2, "Re: Grading Request", "I will round it up this time, but I won't be able to do it again.", "Bro. Thompson"),
+    new Message(3, "Re: Grading Request", "Thank you very much! I won't miss any more assignments after this.", "Anna")
+  ];
 
+  onAddMessage(message: Message) {
+    this.messages.push(message);
+  }
 }
