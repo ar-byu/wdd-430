03b6522f565bd03a13c20ca572aa0c30c1e85974
ar-byu <103947632+ar-byu@users.noreply.github.com>
2023-01-21 11:51:18 -0500

    Added databinding to make contactDetail functional

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Contact } from '../contact.model';
 
 @Component({
@@ -6,11 +6,20 @@ import { Contact } from '../contact.model';
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.css']
 })
+
+
 export class ContactListComponent {
+  @Output() contactClicked = new EventEmitter<Contact>();
+
   contacts: Contact[] = [
     new Contact(1, "R. Kent Jackson", "jacksonk@byui.edu", "208-496-3771", "../../assets/images/jacksonk.jpg",
     null),
     new Contact(2, "Rex Barzee", "barzeer@byui.edu", "208-496-3768", "../../assets/images/barzeer.jpg",
     null)
   ];
+
+  onContactSelected(contact: Contact) {
+    this.contactClicked.emit(contact);
+  }
+  
 }
