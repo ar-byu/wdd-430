d2f4d1ac274daa9accc5bfb75ca13a0774464def
ar-byu <103947632+ar-byu@users.noreply.github.com>
2023-01-27 19:58:38 -0500

    Added dropdown directive, implemented documents

--- cms/src/app/documents/documents-list/documents-list.component.ts
+++ cms/src/app/documents/documents-list/documents-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-documents-list',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./documents-list.component.css']
 })
 export class DocumentsListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
+  documents: Document[] = [
+    new Document(1, 'Grocery List', 'A list of groceries', '/document-items/groceries.txt', null),
+    new Document(2, 'Weekly To-Do', 'Tasks this week', '/document-items/todo.txt', null),
+    new Document(3, 'Semester Schedule', 'Schedule for this semester', '/document-items/semestersched.txt', null),
+    new Document(4, 'Budget and Savings', 'Money management plan', '/document-items/budget.txt', null)
+  ]
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
